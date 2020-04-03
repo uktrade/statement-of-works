34584db2bfad3a04c72c9ee43ef34264eb8bb827
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2020-04-03 16:56:49 +0100

    Remove commented out code

--- sow/admin.py
+++ sow/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import StatementOfWork, Role
 
-#class RoleAdmin(admin.ModelAdmin):
-#    list_display = ('roleName')
-
 # Register your models here.
 admin.site.register(StatementOfWork)
-admin.site.register(Role)#, RoleAdmin)
+admin.site.register(Role)
