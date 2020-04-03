ef0861ae35b9cf35a17535a05e95e4034194f0d6
elcct <pawel@mikesz.com>
2020-04-03 12:05:46 +0100

    Add initial React application.

---
+++ frontend/src/components/PageHeader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import TopNav, { asNavLinkAnchor } from '@govuk-react/top-nav'
+
+const NavAnchor = asNavLinkAnchor(Link)
+
+const ServiceTitle = <NavAnchor to="/">Statement of Works</NavAnchor>
+
+const PageHeader = () => (
+  <div>
+    <TopNav company="" serviceTitle={ServiceTitle} />
+  </div>
+)
+
+export default PageHeader
---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import 'normalize.css'
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './components/App'
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+)
